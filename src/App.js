16d263c8e8fab0310e68b487aebe312aaf17e612
Parker Agee <pagee@pagee.local>
2019-01-08 17:01:06 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Jabba boilerplate</h1>
-                <LambdaDemo />
             </div>
         );
     }
